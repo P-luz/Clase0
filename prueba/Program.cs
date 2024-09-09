2dcd992a5cbedbdec13718ca91283226b508c584
P-luz <luzap200@gmail.com>
2024-09-09 11:51:04 +0000

    Primer Proyecto

---
+++ prueba/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Bienvendios... ");
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.WriteLine("Hello, Debo salir en azul");
