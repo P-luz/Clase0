7b627560c921cc2ca54a249dcf406574b22edcd4
Luz Adriana <luzap200@gmail.com>
2024-09-11 11:40:09 +0200

    Variables

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,5 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.BackgroundColor = ConsoleColor.Blue;
-Console.Write("***Congratulations***");
-Console.Write("  :) ");
-Console.Write("You wrote your first line of code");
+﻿string firstName1;
+firstName1 = "Bob";
+Console.WriteLine (firstName1);
+Console.WriteLine ("********************************");
+string firstName;
+firstName = "Bob";
+Console.WriteLine(firstName);
+firstName = "Liem";
+Console.WriteLine(firstName);
+firstName = "Isabella";
+Console.WriteLine(firstName);
+firstName = "Yasmin";
+Console.WriteLine(firstName);
+Console.WriteLine ("********************************");
+string firstName2 = "Luz Adriana";
+Console.WriteLine(firstName2);
+Console.WriteLine ("********************************");
+//var message = "Hello world!";
+Console.WriteLine ("**************DESAFIO******************");
+string firstName3;
+firstName3 = "Bob";
+//Console.WriteLine (firstName);
+int num=3;
+//Console.WriteLine (num);
+//float tem = 34.4f;
+//Console.WriteLine (tem);
+var tem2 = 34.4;
+//Console.WriteLine (tem2);
+Console.WriteLine ("Hello,"+ firstName3 + ",! You have"+ num + "messages in your inbox. The temperature is"+tem2+ "celsius.");
+
+
+
+
