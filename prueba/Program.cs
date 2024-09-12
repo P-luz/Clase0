0b889d9012bff4dd4e46e61b0b8b19d1fad632a8
Luz Adriana <luzap200@gmail.com>
2024-09-12 13:55:56 +0200

    Ejercicio de notas

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -74,3 +74,43 @@ Console.WriteLine ("The temperature is: "+Math.Round(Celsius,1)+ " Celsius");
 Console.WriteLine ("The temperature is: "+Celsius.ToString("0.00")+ " Celsius");
 Console.WriteLine ("The temperature is: "+Celsius.ToString("#.##")+ " Celsius");
 //Console.WriteLine ("The temperature is: "+decimal.Round(Celsius,1)+ " Celsius");
+
+
+Console.Clear();
+Console.WriteLine("\n*******Cálculo de notas*******");
+int currentAssignments = 5;
+
+int sophia1 = 93;
+int sophia2 = 87;
+int sophia3 = 98;
+int sophia4 = 95;
+int sophia5 = 100;
+
+int nicolas1 = 80;
+int nicolas2 = 83;
+int nicolas3 = 82;
+int nicolas4 = 88;
+int nicolas5 = 85;
+
+int zahirah1 = 84;
+int zahirah2 = 96;
+int zahirah3 = 73;
+int zahirah4 = 85;
+int zahirah5 = 79;
+
+int jeong1 = 90;
+int jeong2 = 92;
+int jeong3 = 98;
+int jeong4 = 100;
+int jeong5 = 97;
+
+Console.WriteLine("\n*******Notas*******");
+Console.WriteLine("Sophia:\t\t"+sophia1+"\t"+sophia2+"\t"+sophia3+"\t"+sophia4+"\t"+sophia5);  
+Console.WriteLine("Nicolas:\t"+nicolas1+"\t"+nicolas2+"\t"+nicolas3+"\t"+nicolas4+"\t"+nicolas5);
+Console.WriteLine("Zahirah::\t"+zahirah1+"\t"+zahirah2+"\t"+zahirah3+"\t"+zahirah4+"\t"+zahirah5);
+Console.WriteLine("Jeong\t\t"+jeong1+"\t"+jeong2+"\t"+jeong3+"\t"+jeong4+"\t"+jeong5); 
+Console.WriteLine("\n*******Promedios del estudiante*******");
+Console.WriteLine("Sophia:\t\t"+((sophia1+sophia2+sophia3+sophia4+sophia5)/5).ToString("0.00"));  
+Console.WriteLine("Nicolas:\t"+((nicolas1+nicolas2+nicolas3+nicolas4+nicolas5)/5).ToString("0.00"));
+Console.WriteLine("Zahirah::\t"+((zahirah1+zahirah2+zahirah3+zahirah4+zahirah5)/5).ToString("0.00"));
+Console.WriteLine("Jeong\t\t"+((jeong1+jeong2+jeong3+jeong4+jeong5)/5).ToString("0.00"));
--- prueba/obj/Debug/net8.0/prueba.AssemblyInfo.cs
+++ prueba/obj/Debug/net8.0/prueba.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("prueba")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5559edec92d496f837d734160e4bd501b1c039bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3abd5342548cfeb79a3864e1bc17ba7062651d74")]
 [assembly: System.Reflection.AssemblyProductAttribute("prueba")]
 [assembly: System.Reflection.AssemblyTitleAttribute("prueba")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
